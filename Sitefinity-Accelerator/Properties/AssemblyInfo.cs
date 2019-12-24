16e62eb6b253d88475ca5d32bdcb6023f7b5931a
Michael Vickers <mavickers@gmail.com>
2019-12-24 12:14:18 -0500

    Add AssemblyInfo.

---
+++ Sitefinity-Accelerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("LightPath Solutions, LLC")]
+[assembly: AssemblyProduct("Sitefinity Accelerator")]
+[assembly: AssemblyCopyright("Copyright © 2019 LightPath Solutions, LLC. All rights reserved.")]
+[assembly: AssemblyTrademark("Sitefinity Accelerator")]
+[assembly: AssemblyTitle("Sitefinity Accelerator")]
+[assembly: AssemblyDescription("Interfaces and classes for scaffolding Sitefinity projects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4384B738-D253-466C-BDF7-352850BABD70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("12.1.7128.0")]
+[assembly: AssemblyFileVersion("12.1.7128.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
