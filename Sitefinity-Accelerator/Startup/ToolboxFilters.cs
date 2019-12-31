9de95c6c2605a597c6beecdd0eb2cde4ff8779e2
Michael Vickers <mavickers@gmail.com>
2019-12-31 12:21:08 -0500

    Add toolbox filters warmup;  cleanup.

---
+++ Sitefinity-Accelerator/Startup/ToolboxFilters.cs
@@ -0,0 +1,23 @@
+﻿using SitefinityAccelerator.Configuration.Security.Widgets;
+using Telerik.Microsoft.Practices.Unity;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Configuration;
+
+namespace SitefinityAccelerator.Startup
+{
+    public static class ToolboxFilters
+    {
+        public static void Warmup()
+        {
+            if (ObjectFactory.Container != null)
+            {
+                // filters for toolbox widgets sometimes do not execute until they are read; apparently
+                // the process of loading a page or template in the editor does not read the settings
+                // by itself every time; the circumstances where the filters fail to execute are not
+                // completely know at this time, so leaving this here in case it's needed.
+
+                ObjectFactory.Container.Resolve(typeof(ConfigElementList<WidgetSecurityElement>));
+            }
+        }
+    }
+}
